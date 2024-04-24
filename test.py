b731c79396c834945a1af9144a22768865beafec
K1000Egg <168023158+K1000Egg@users.noreply.github.com>
2024-04-24 22:28:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print ("Hello World!")
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+            
+        
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+if word in meme_dict.keys():
+    print (word, "kelimesinin anlamı = ", meme_dict[word])
+else:
+    print("Maalesef verdiğiniz kelime listemde yok!")
